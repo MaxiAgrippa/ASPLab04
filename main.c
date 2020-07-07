38550c42478ff0cb9dc03efde13da2dc2d31712e
MaxiAgrippa <maxiagrippamail@mail.com>
2020-07-07 00:52:41 -0400

    v1.0 final

---
+++ main.c
@@ -0,0 +1,96 @@
+//
+// Created by Maix on 7/6/20.
+//
+#include "stdio.h"
+#include "stdlib.h"
+
+// flip the picture
+void reversePicture(FILE *filePointer_1, FILE *filePointer_2, int columns, int rows, long int fileSize);
+
+/**
+ * reverse the target .pgm picture and store to another file.
+ * @param argc
+ * @param argv
+ * @return
+ */
+int main(int argc, char *argv[])
+{
+    // two file pointer, one for source, one for write
+    FILE *filePointer_1;
+    FILE *filePointer_2;
+    // file size
+    long int fileSize;
+    // serve for reading source file head information
+    char temp[100];
+    // picture columns and rows
+    int columns, rows;
+    // open the source file to read
+    filePointer_1 = fopen(argv[1], "r");
+    // if failed, show error and exit
+    if (filePointer_1 == NULL)
+    {
+        fprintf(stderr,"Can not open file %s.\n",argv[1]);
+        exit(0);
+    }
+    // open the target file to write
+    filePointer_2 = fopen(argv[2], "w");
+    // if failed, show error and exit
+    if (filePointer_2 == NULL)
+    {
+        fprintf(stderr,"Can not open file %s.\n",argv[2]);
+        exit(0);
+    }
+
+    // put the stream beginning to the end of the source file
+    fseek(filePointer_1, 0, SEEK_END);
+    // get file size.
+    fileSize = ftell(filePointer_1);
+    // set to the beginning of the stream
+    rewind(filePointer_1);
+
+    // get source file type
+    fgets(temp, 100, filePointer_1);
+    // put file type to target file.
+    fputs(temp, filePointer_2);
+
+    // get source file picture columns and rows.
+    fscanf(filePointer_1, "%d %d\n", &columns, &rows);
+    // put picture columns and rows into the target file.
+    fprintf(filePointer_2, "%d %d\n", columns, rows);
+
+    // get source file picture max grey.
+    fgets(temp, 100, filePointer_1);
+    // put picture max grey into target file.
+    fputs(temp, filePointer_2);
+
+    // reverse the picture.
+    reversePicture(filePointer_1, filePointer_2, columns, rows, fileSize);
+
+    // close file pointers
+    fclose(filePointer_1);
+    fclose(filePointer_2);
+}
+
+// reverse the picture.
+void reversePicture(FILE *filePointer_1, FILE *filePointer_2, int columns, int rows, long int fileSize)
+{
+    // store one row of pixels.
+    char oneRowOfPicture[columns];
+    // set the stream beginner to the end of the file.
+    fseek(filePointer_2, fileSize, SEEK_SET);
+
+    // for every rows
+    for (int i = 0; i < rows; i++)
+    {
+        // read one row of pixels, each pixel is 1 byte.
+        fread(oneRowOfPicture, 1, columns, filePointer_1);
+        // set the stream beginner to columns a head of current stream position.
+        fseek(filePointer_2, -columns, SEEK_CUR);
+        // write one row of picture at the end row of the target file.
+        // it's reversed since it go through buffer which is a first in last out.
+        fwrite(oneRowOfPicture, 1, columns, filePointer_2);
+        // reset the stream beginner to columns a head of current stream position,
+        // since fwrite() will move the position behind.
+        fseek(filePointer_2, -columns, SEEK_CUR);
+    }
+}
--- reversePicture.c
+++ reversePicture.c
@@ -3,9 +3,13 @@
 //
 #include "stdio.h"
 #include "stdlib.h"
+#include "sys/fcntl.h"
+#include "sys/types.h"
+#include "sys/stat.h"
+#include "unistd.h"
 
-// flip the picture
-void reversePicture(FILE *filePointer_1, FILE *filePointer_2, int columns, int rows, long int fileSize);
+// reverse the picture.
+void reversePicture(int file_1, int file_2, int columns, int rows, long int fileSize);
 
 /**
  * reverse the target .pgm picture and store to another file.
@@ -15,81 +19,90 @@ void reversePicture(FILE *filePointer_1, FILE *filePointer_2, int columns, int r
  */
 int main(int argc, char *argv[])
 {
-    // two file pointer, one for source, one for write
-    FILE *filePointer_1;
-    FILE *filePointer_2;
     // file size
     long int fileSize;
     // serve for reading source file head information
-    char temp[100];
+    char *temp = (char *) calloc(100, sizeof(char));
     // picture columns and rows
     int columns, rows;
-    // open the source file to read
-    filePointer_1 = fopen(argv[1], "r");
+    // open source file
+    int file_1 = open(argv[1], O_RDONLY);
     // if failed, show error and exit
-    if (filePointer_1 == NULL)
+    if (file_1 == -1)
     {
-        fprintf(stderr,"Can not open file %s.\n",argv[1]);
+        fprintf(stderr, "Can not open file %s.\n", argv[1]);
         exit(0);
     }
-    // open the target file to write
-    filePointer_2 = fopen(argv[2], "w");
+    // open target file
+    int file_2 = open(argv[2], O_WRONLY | O_CREAT);
     // if failed, show error and exit
-    if (filePointer_2 == NULL)
+    if (file_2 == -1)
     {
-        fprintf(stderr,"Can not open file %s.\n",argv[2]);
+        fprintf(stderr, "Can not open file %s.\n", argv[1]);
         exit(0);
     }
-    // put the stream beginning to the end of the source file
-    fseek(filePointer_1, 0, SEEK_END);
-    // get file size.
-    fileSize = ftell(filePointer_1);
+
+    // put the stream beginning to the end of the source file, and get file size.
+    fileSize = lseek(file_1, 0, SEEK_END);
     // set to the beginning of the stream
-    rewind(filePointer_1);
+    lseek(file_1, 0, SEEK_SET);
 
     // get source file type
-    fgets(temp, 100, filePointer_1);
+    read(file_1, temp, 3);
     // put file type to target file.
-    fputs(temp, filePointer_2);
+    write(file_2, temp, 3);
+
+    // reset temp;
+    free(temp);
+    temp = (char *) calloc(100, sizeof(char));
 
     // get source file picture columns and rows.
-    fscanf(filePointer_1, "%d %d\n", &columns, &rows);
-    // put picture columns and rows into the target file.
-    fprintf(filePointer_2, "%d %d\n", columns, rows);
+    FILE *file = fopen(argv[1], "r");
+    fseek(file, 3, SEEK_SET);
+    fscanf(file, "%d %d\n", &columns, &rows);
+    // put picture max grey into target file.
+    sprintf(temp, "%d %d\n", columns, rows);
+    write(file_2, temp, 8);
+    // reset stream position to the correct position.
+    lseek(file_1, 8, SEEK_CUR);
+
+    // reset temp;
+    free(temp);
+    temp = (char *) calloc(100, sizeof(char));
 
     // get source file picture max grey.
-    fgets(temp, 100, filePointer_1);
+    read(file_1, temp, 4);
     // put picture max grey into target file.
-    fputs(temp, filePointer_2);
+    write(file_2, temp, 4);
 
     // reverse the picture.
-    reversePicture(filePointer_1, filePointer_2, columns, rows, fileSize);
+    reversePicture(file_1, file_2, columns, rows, fileSize);
 
-    // close file pointers
-    fclose(filePointer_1);
-    fclose(filePointer_2);
+    // close file
+    close(file_1);
+    close(file_2);
 }
 
 // reverse the picture.
-void reversePicture(FILE *filePointer_1, FILE *filePointer_2, int columns, int rows, long int fileSize)
+void reversePicture(int file_1, int file_2, int columns, int rows, long int fileSize)
 {
     // store one row of pixels.
     char oneRowOfPicture[columns];
     // set the stream beginner to the end of the file.
-    fseek(filePointer_2, fileSize, SEEK_SET);
+    lseek(file_2, fileSize, SEEK_SET);
 
     // for every rows
     for (int i = 0; i < rows; i++)
     {
         // read one row of pixels, each pixel is 1 byte.
-        fread(oneRowOfPicture, 1, columns, filePointer_1);
+        read(file_1, oneRowOfPicture, columns);
         // set the stream beginner to columns a head of current stream position.
-        fseek(filePointer_2, -columns, SEEK_CUR);
+        lseek(file_2, -columns, SEEK_CUR);
         // write one row of picture at the end row of the target file.
         // it's reversed since it go through buffer which is a first in last out.
-        fwrite(oneRowOfPicture, 1, columns, filePointer_2);
+        write(file_2, oneRowOfPicture, columns);
         // reset the stream beginner to columns a head of current stream position,
         // since fwrite() will move the position behind.
-        fseek(filePointer_2, -columns, SEEK_CUR);
+        lseek(file_2, -columns, SEEK_CUR);
     }
 }
